cae7646588b164079bc7a438aaabeb6b9c667689
itsuki <itsuki.enjyoy@gmail.com>
2024-06-28 15:14:23 +0900

    Initial Commit

---
+++ ViewManipulationDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ViewManipulationDemo
+//
+//  Created by Itsuki on 2024/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ViewManipulationDemo/ViewManipulationDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewManipulationDemoApp.swift
+//  ViewManipulationDemo
+//
+//  Created by Itsuki on 2024/06/28.
+//
+
+import SwiftUI
+
+@main
+struct ViewManipulationDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
